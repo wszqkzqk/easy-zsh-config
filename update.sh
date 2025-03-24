2d0d6114d501e0df917efe3ea2947e78ba85f805
Zhou Qiankang <wszqkzqk@qq.com>
2025-03-24 11:20:58 +0800

    Update scripts to update
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-cd `dirname $0`; pwd
-updpkgsums
-makepkg --printsrcinfo > .SRCINFO
-git add $(git ls-files)
-git commit -s -m 'Update to a new version'
-git push
-git push github
+cd $(dirname $0); pwd \
+&&updpkgsums \
+&&makepkg --printsrcinfo > .SRCINFO \
+&&git add $(git ls-files) \
+&&git commit -s -m 'Update to a new version' \
+&&git push \
+&&git push github
