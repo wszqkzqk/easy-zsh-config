5d8a7aff01ec38412c402cce552a3e4a1b6f3c73
Zhou Qiankang <wszqkzqk@qq.com>
2025-03-24 11:19:09 +0800

    Update scripts to update
    Signed-off-by: Zhou Qiankang <wszqkzqk@qq.com>

--- update.sh
+++ update.sh
@@ -2,8 +2,7 @@
 cd `dirname $0`; pwd
 updpkgsums
 makepkg --printsrcinfo > .SRCINFO
-git add PKGBUILD .SRCINFO update.sh zshrc LICENSE easy-zsh-config.install README.md easy-zsh-config
+git add $(git ls-files)
 git commit -s -m 'Update to a new version'
 git push
 git push github
-
